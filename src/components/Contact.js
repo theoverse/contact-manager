6f2263e5644294752532bd6bdba82fa3ff969412
theoverse <theoverse@protonmail.com>
2022-02-13 11:49:11 -0800

    typechecking with proptypes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Contact extends Component {
+    // static propTypes = {
+    //     name: PropTypes.string.isRequired,
+    //     email: PropTypes.string.isRequired,
+    //     phone: PropTypes.string.isRequired
+    // }
+
     render() {
         const { name, email, phone } = this.props
         return (
@@ -13,6 +20,12 @@ class Contact extends Component {
             </div>
         )
     }
+}
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired
 }
 
 export default Contact
