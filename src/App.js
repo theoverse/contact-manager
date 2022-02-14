5fd5181cada62010894666fafbb0dcfcec7b68cb
theoverse <theoverse@protonmail.com>
2022-02-14 08:40:21 -0800

    setup react router and added about component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Contacts from './components/contacts/Contacts'
-import Header from './components/layout/Header'
 import AddContact from './components/contacts/AddContact'
+import Header from './components/layout/Header'
+import About from './components/pages/About'
+
 import { Provider } from './context'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,13 +14,18 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Routes>
+                <Route exact path="/" element={<Contacts />} />
+                <Route exact path="/contact/add" element={<AddContact />} />
+                <Route exact path="/about" element={<About />} />
+              </Routes>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     )
   }
