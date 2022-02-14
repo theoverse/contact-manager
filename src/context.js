8b23df1c43c797cf7a3059ae5ec6ccf9f1033a98
theoverse <theoverse@protonmail.com>
2022-02-14 05:12:33 -0800

    added a context reducer for actions and a delete action

--- src/context.js
+++ src/context.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 
 const Context = React.createContext()
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'DELETE_CONTACT':
+            return {
+                ...state,
+                contacts: state.contacts.filter(contact =>
+                    contact.id !== action.payload)
+            }
+        default:
+            return state
+    }
+}
+
 export class Provider extends Component {
     state = {
         contacts: [
@@ -23,7 +36,9 @@ export class Provider extends Component {
                 email: "triddle@gmail.com",
                 phone: "777-777-7777"
             }
-        ]
+        ],
+        dispatch: action => this.setState(state =>
+            reducer(state, action))
     }
 
     render() {
