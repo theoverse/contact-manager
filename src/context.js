4c8bb68fe58388a08222ebecbd9bc6fadc74a2d6
theoverse <theoverse@protonmail.com>
2022-02-14 07:20:00 -0800

    added form submit action to context

--- src/context.js
+++ src/context.js
@@ -10,6 +10,11 @@ const reducer = (state, action) => {
                 contacts: state.contacts.filter(contact =>
                     contact.id !== action.payload)
             }
+        case 'ADD_CONTACT':
+            return {
+                ...state,
+                contacts: [action.payload, ...state.contacts]
+            }
         default:
             return state
     }
