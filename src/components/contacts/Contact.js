f6ba3db26340efb5560a76bcf87e741e320299f1
theoverse <theoverse@protonmail.com>
2022-02-14 14:34:55 -0800

    added editcontact component and fetch contacts

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { Consumer } from '../../context'
 import axios from 'axios'
@@ -45,6 +46,17 @@ class Contact extends Component {
                                         this, id, dispatch
                                     )}
                                 />
+                                <Link to={`contact/edit/${id}`}>
+                                    <i
+                                        className="fas fa-pencil-alt fa-sm"
+                                        style={{
+                                            cursor: 'pointer',
+                                            float: 'right',
+                                            color: 'black',
+                                            marginRight: '1rem'
+                                        }}
+                                    />
+                                </Link>
                             </h4>
                             {showContactInfo ? (
                                 <ul className='list-group'>
