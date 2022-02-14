80606ebd00c9a531e93eb2a515211d2dc25799a3
theoverse <theoverse@protonmail.com>
2022-02-14 14:01:03 -0800

    refactored axios requests with async and await

--- src/context.js
+++ src/context.js
@@ -30,9 +30,11 @@ export class Provider extends Component {
             reducer(state, action))
     }
 
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(res => this.setState({ contacts: res.data }))
+    async componentDidMount() {
+        const res = await axios
+            .get('https://jsonplaceholder.typicode.com/users')
+
+        this.setState({ contacts: res.data })
     }
 
     render() {
